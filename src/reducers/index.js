8e1cd437b77de7e2375611981748bfd340803a40
unknown <jordangoulet13@gmail.com>
2020-11-30 20:10:39 -0800

    added names to posts

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
 export default combineReducers({
   // can use a dummy key as boiler plate to run
   // without initial errors.
   // i.e replaceMe: () => "hi there"
+  users: usersReducer,
   posts: postsReducer,
 });
